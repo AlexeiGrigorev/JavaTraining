7ced7f18a4621dba88b413fe33d872ff9426c951
Алексей Григорьев <ilovemeet2022@mail.com>
2022-10-20 20:36:24 +0300

    удалил cllass Theend

--- src/com/github/alexeigrigorev/javatraining/lesson2/TheEnd.java
+++
@@ -1,9 +0,0 @@
-package com.github.alexeigrigorev.javatraining.lesson2;
-
-public class TheEnd {
-    public static void main(String[] Args) {
-        System.out.println("Начинаем сборку");
-        IAssemblyLine line = new TankIAssemblyLIne();
-        IProduct product =  line.assembleProduct();
-    }
-}
